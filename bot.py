92f98b983e46fc5da5b28b03fb1fde62477bdaa8
itzraiyan <mdzilhazzarefin@gmail.com>
2024-11-13 17:16:02 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import logging
 from flask import Flask, request
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from handlers import commands, message, error
-from config import API_ID, API_HASH, TOKEN
+from config import API_ID, API_HASH, TOKEN, SOURCE_CHANNEL_ID, DESTINATION_CHANNEL_ID
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -10,17 +11,22 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+# Initialize bot forwarding status
+is_forwarding = False
+
 def main():
+    global is_forwarding
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
     # Command handlers
     dp.add_handler(CommandHandler("start", commands.start))
     dp.add_handler(CommandHandler("help", commands.help))
+    dp.add_handler(CommandHandler("start_forwarding", commands.start_forwarding))
+    dp.add_handler(CommandHandler("stop_forwarding", commands.stop_forwarding))
 
     # Message handler
-    dp.add_handler(MessageHandler(Filters.chat(commands.SOURCE_CHANNEL_ID), message.forward_message))
-    dp.add_handler(MessageHandler(Filters.update.edited_message, message.edit_message))
+    dp.add_handler(MessageHandler(Filters.chat(SOURCE_CHANNEL_ID) & Filters.all, message.forward_message))
 
     # Error handler
     dp.add_error_handler(error.error)
